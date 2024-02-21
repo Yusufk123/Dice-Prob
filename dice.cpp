4c89dc177dcd6cd82279093db38011e10ed0bf18
Sal <yusufk1230@gmail.com>
2024-02-21 16:06:14 -0500

    vscode

--- dice.cpp
+++ dice.cpp
@@ -4,12 +4,10 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const string& word : msg)
-    {
+    for (const string& word : msg){
         cout << word << " ";
     }
     cout << endl;
